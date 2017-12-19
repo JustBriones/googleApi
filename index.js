156872be02a63d364a1c24b3537a73a3876542ec
Justin Briones <justbriones@gmail.com>
2017-12-18 19:01:13 -0800

    added media query and adjusted margin. also added https solution

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ $(document).ready(function(){
       var temp = data.main.temp
       var weather = data.weather[0].description
       var city = data.name
-      $(".location1").append(`<div class='row row-item city'>${city}</div>`)
+      $(".rowLocation1").append(`<div class='row row-item city'>${city}</div>`)
       $(".location1").append(`<div class='row row-item weather'>${weather}</div>`)
       $(".location1").append(`<div class='row row-item temp'>${temp} &deg;
       F</div>`)
@@ -20,7 +20,7 @@ $(document).ready(function(){
 //submit2 button functionality
 $("#submit2").click(function() {
   var cityTwo = $("#cityTwo").val()
-  $.getJSON(`http://api.openweathermap.org/data/2.5/weather?APPID=cd6b90ba80057d06e230432ecdf83865&units=imperial&q=${cityTwo}`, (data) => {
+  $.getJSON(`https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/weather?APPID=cd6b90ba80057d06e230432ecdf83865&units=imperial&q=${cityTwo}`, (data) => {
     var temp = data.main.temp
     var weather = data.weather[0].description
     var city = data.name
@@ -36,7 +36,7 @@ $("#submit2").click(function() {
 //submit3 button functionality
 $("#submit3").click(function() {
   var cityThree = $("#cityThree").val()
-  $.getJSON(`http://api.openweathermap.org/data/2.5/weather?APPID=cd6b90ba80057d06e230432ecdf83865&units=imperial&q=${cityThree}`, (data) => {
+  $.getJSON(`https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/weather?APPID=cd6b90ba80057d06e230432ecdf83865&units=imperial&q=${cityThree}`, (data) => {
     var temp = data.main.temp
     var weather = data.weather[0].description
     var city = data.name
